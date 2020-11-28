b0c00d0a247650f14151f1df49a243ba8cf17942
ahmedelarabykhaled <ahmedelarabykhaled2020@yahoo.com>
2020-11-28 13:29:38 +0200

    fix

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -3,28 +3,29 @@
 
     <div class="container">
         <div class="row">
-            <div class="col-lg-3 col-md-4 col-sm-6">
-                <div class="oil-clients border p-5 text-center rounded "
-                     style="background-color: rgb({{ rand(0,255) }}, {{ rand(0,255) }}, {{ rand(0,255) }})">
-                    <h4>كل العملاء</h4>
-                    <h1>{{ \App\Models\OilClient::get()->count() + \App\Models\TrashClient::get()->count()  }}</h1>
-                </div>
-            </div>
-            <div class="col-lg-3 col-md-4 col-sm-6">
-                <div class="oil-clients border p-5 text-center rounded"
-                     style="background-color: rgb({{ rand(0,255) }}, {{ rand(0,255) }}, {{ rand(0,255) }})">
-                    <h4>المشتركين فى الزيت</h4>
-                    <h1>{{ \App\Models\OilClient::get()->count() }}</h1>
-                </div>
-            </div>
-            <div class="col-lg-3 col-md-4 col-sm-6">
-                <div class="oil-clients border p-5 text-center rounded"
-                     style="background-color: rgb({{ rand(0,255) }}, {{ rand(0,255) }}, {{ rand(0,255) }})">
-                    <h4>المشتركين فى القمامة</h4>
-                    <h1>{{ \App\Models\TrashClient::get()->count() }}</h1>
-                </div>
-            </div>
             @if(auth()->user()->role->role_id == 1)
+                <div class="col-lg-3 col-md-4 col-sm-6">
+                    <div class="oil-clients border p-5 text-center rounded "
+                         style="background-color: rgb({{ rand(0,255) }}, {{ rand(0,255) }}, {{ rand(0,255) }})">
+                        <h4>كل العملاء</h4>
+                        <h1>{{ \App\Models\OilClient::get()->count() + \App\Models\TrashClient::get()->count()  }}</h1>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-4 col-sm-6">
+                    <div class="oil-clients border p-5 text-center rounded"
+                         style="background-color: rgb({{ rand(0,255) }}, {{ rand(0,255) }}, {{ rand(0,255) }})">
+                        <h4>المشتركين فى الزيت</h4>
+                        <h1>{{ \App\Models\OilClient::get()->count() }}</h1>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-4 col-sm-6">
+                    <div class="oil-clients border p-5 text-center rounded"
+                         style="background-color: rgb({{ rand(0,255) }}, {{ rand(0,255) }}, {{ rand(0,255) }})">
+                        <h4>المشتركين فى القمامة</h4>
+                        <h1>{{ \App\Models\TrashClient::get()->count() }}</h1>
+                    </div>
+                </div>
+
                 <div class="col-lg-3 col-md-4 col-sm-6">
                     <div class="oil-clients border p-5 text-center rounded"
                          style="background-color: rgb({{ rand(0,255) }}, {{ rand(0,255) }}, {{ rand(0,255) }})">
